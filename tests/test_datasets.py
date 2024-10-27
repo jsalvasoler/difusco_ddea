c9514de52078f7127747886f1b4e5f1288c2e73a
Joan Salva <e12223411@conan.ac.tuwien.ac.at>
2024-10-27 18:11:40 +0100

    feat: add mis dataset code

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,3 +1,8 @@
+import os
+
+import numpy as np
+
+from difusco.mis.mis_dataset import MISDataset
 from difusco.tsp.tsp_graph_dataset import TSPGraphDataset
 
 
@@ -31,6 +36,113 @@ def test_tsp_dataset_sample() -> None:
         assert adj_matrix[tour[i], tour[i + 1]] == 1
 
 
+def test_mis_dataset_is_loaded() -> None:
+    dataset = MISDataset(
+        data_file="tests/resources/er_example_dataset/*.gpickle",
+    )
+    assert len(dataset) == 2
+
+
+def test_mis_dataset_sample() -> None:
+    dataset = MISDataset(
+        data_file="tests/resources/er_example_dataset/*.gpickle",
+    )
+
+    sample = dataset.__getitem__(0)
+
+    assert len(sample) == 3
+
+    # First element is the idx
+    assert sample[0].shape == (1,)
+    assert sample[0].item() == 0
+
+    # Second element is the graph data
+    graph_data = sample[1]
+    n = graph_data.num_nodes
+    m = graph_data.num_edges
+    assert graph_data.x.shape == (n,)
+    assert graph_data.edge_index.shape == (2, m)
+    # True labels
+    assert 0 <= np.sum(graph_data.x.numpy()) <= n
+
+    # Third element is the size indicator
+    assert sample[2].shape == (1,)
+    assert sample[2].item() == n
+
+    return sample
+
+
+def test_mis_real_test_sample() -> None:
+    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    dataset = MISDataset(
+        data_file=f"{root_dir}/data/mis/er_test/*.gpickle",
+    )
+    assert len(dataset) == 128
+
+    sample = dataset.__getitem__(0)
+
+    assert len(sample) == 3
+
+    # First element is the idx
+    assert sample[0].shape == (1,)
+    assert sample[0].item() == 0
+
+    # Second element is the graph data
+    graph_data = sample[1]
+    n = graph_data.num_nodes
+    m = graph_data.num_edges
+    assert graph_data.x.shape == (n,)
+    assert graph_data.edge_index.shape == (2, m)
+    # True labels
+    true_labels = np.sum(graph_data.x.numpy())
+    print(true_labels)
+    assert 0 <= true_labels <= n
+
+    # Third element is the size indicator
+    assert sample[2].shape == (1,)
+    assert sample[2].item() == n
+
+    return sample
+
+
+def test_mis_real_train_sample() -> None:
+    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    dataset = MISDataset(
+        data_file=f"{root_dir}/data/mis/er_train/*.gpickle",
+        data_label_dir=f"{root_dir}/data/mis/er_train/preprocessed/kamis",
+    )
+    assert len(dataset) == 163840
+
+    sample = dataset.__getitem__(0)
+
+    assert len(sample) == 3
+
+    # First element is the idx
+    assert sample[0].shape == (1,)
+    assert sample[0].item() == 0
+
+    # Second element is the graph data
+    graph_data = sample[1]
+    n = graph_data.num_nodes
+    m = graph_data.num_edges
+    assert graph_data.x.shape == (n,)
+    assert graph_data.edge_index.shape == (2, m)
+    # True labels
+    true_labels = np.sum(graph_data.x.numpy())
+    print(true_labels)
+    assert 0 < true_labels <= n, f"true_labels: {true_labels}"
+
+    # Third element is the size indicator
+    assert sample[2].shape == (1,)
+    assert sample[2].item() == n
+
+    return sample
+
+
 if __name__ == "__main__":
-    test_tsp_dataset_is_loaded()
-    test_tsp_dataset_sample()
+    # test_tsp_dataset_is_loaded()
+    # test_tsp_dataset_sample()
+    test_mis_real_test_sample()
+    test_mis_real_train_sample()
