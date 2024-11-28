651fe5637e065d5bedfba8621c1383b19de094b9
Joan Salva <e12223411@conan.ac.tuwien.ac.at>
2024-11-28 14:32:29 +0100

    chore: update trick

--- src/difusco_edward_sun/data/mis-benchmark-framework/do_the_lil_trick.sh
+++ src/difusco_edward_sun/data/mis-benchmark-framework/do_the_lil_trick.sh
@@ -1,7 +1,14 @@
 # Script to transfer the labeled graphs (1) to (0), and move the ones from (0) that have not been labeled to (1)
 
+# Get labeled graphs from eowyn server
+scp e12223411@eowyn.ac.tuwien.ac.at:/home1/e12223411/repos/difusco/data/mis/er_train_annotations_1.zip .
+unzip er_train_annotations_1.zip
+
 # Move the labeled graphs from (1) to (0)
 mv /home/e12223411/repos/difusco/data/mis/er_train_annotations_1/* /home/e12223411/repos/difusco/data/mis/er_train_annotations_0/
+
+# empty er_train_1
+rm -rf /home/e12223411/repos/difusco/data/mis/er_train_1/*
 
 # Move the unlabeled graphs from (0) to (1)
 cd src/difusco_edward_sun/data/mis-benchmark-framework
@@ -15,3 +22,6 @@ echo "Number of files in er_train_1: $(ls /home/e12223411/repos/difusco/data/mis
 
 # Finally zip the data/mis/er_train_1 file
 zip -r /home/e12223411/repos/difusco/data/mis/er_train_1.zip /home/e12223411/repos/difusco/data/mis/er_train_1
+
+# And send it to the eowyn server
+scp /home/e12223411/repos/difusco/data/mis/er_train_1.zip e12223411@eowyn.ac.tuwien.ac.at:/home1/e12223411/repos/difusco/data/mis/
