034067e3607e20b69a9b134aa4208111c47c1bc0
Joan Salva <e12223411@conan.ac.tuwien.ac.at>
2025-01-03 20:19:51 +0100

    feat: some more things

--- src/difusco_edward_sun/data/mis-benchmark-framework/lil_trick.py
+++ src/difusco_edward_sun/data/mis-benchmark-framework/lil_trick.py
@@ -96,8 +96,55 @@ def check_for_gaps():
     print(f"missing in train_ann: {missing}")
 
 
+def transfer_test_to_train():
+    base = '/home/e12223411/repos/difusco/data/mis'
+    test = os.path.join(base, 'er_1300_1500/test')
+    test_labels = os.path.join(base, 'er_1300_1500/test_labels')
+    train = os.path.join(base, 'er_1300_1500/train')
+    train_labels = os.path.join(base, 'er_1300_1500/train_labels')
+
+    def extract_id(s):
+        return int(s.split(".")[-2].split("_")[-1])
+
+    for f in tqdm(os.listdir(test)):
+        if not f.endswith('.gpickle'):
+            print(f"skipping {f} because it is not a graph")
+            continue
+        id_ = extract_id(f)
+        assert 0 <= id_ <= 39000
+        if id_ < 128:
+            print(f"skipping {f} because id_ < 128")
+            continue
+        filepath = os.path.join(test, f)
+        assert os.path.exists(filepath)
+        shutil.move(filepath, os.path.join(train, f))
+        label_file = f.replace(".gpickle", "_unweighted.result")
+        label_filepath = os.path.join(test_labels, label_file)
+        print(label_filepath)
+        assert os.path.exists(label_filepath)
+        shutil.move(label_filepath, os.path.join(train_labels, label_file))
+    
+    print(len(os.listdir(test)))
+    print(len(os.listdir(test_labels)))
+    print(len(os.listdir(train)))
+    print(len(os.listdir(train_labels)))
+
+def make_sure_all_labels_exist():
+    base = '/home/e12223411/repos/difusco/data/mis'
+    train = os.path.join(base, 'er_300_400/test')
+    train_labels = os.path.join(base, 'er_300_400/test_labels')
+    for f in tqdm(os.listdir(train)):
+        if not f.endswith('.gpickle'):
+            print(f"skipping {f} because it is not a graph")
+            continue
+        label_file = f.replace(".gpickle", "_unweighted.result")
+        assert os.path.exists(os.path.join(train_labels, label_file)), f"label {label_file} does not exist"
+    print("all labels exist")
+
 if __name__ == '__main__':
     # move_annotations()
-    transfer_0_to_1()
+    # transfer_0_to_1()
     # rename_train_degree_labels()
     # check_for_gaps()
+    # transfer_test_to_train()
+    make_sure_all_labels_exist()
