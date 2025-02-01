94bddcf0a547209c60c57884ac1e73b82d82b3fd
Joan Salva <e12223411@eowyn.ac.tuwien.ac.at>
2025-02-01 21:58:44 +0100

    fix: fix metadata of data generation (again)

--- src/ea/generate_difuscombination_samples.py
+++ src/ea/generate_difuscombination_samples.py
@@ -228,8 +228,8 @@ def run_training_data_generation(config: Config) -> None:
         "timestamp": timestamp,
         "config": config.__dict__,
         "runtime": time.time() - start_time,
-        "num_graph_instances": len(dataloader),
-        "runtime_per_instance": (time.time() - start_time) / len(dataloader),
+        "num_graph_instances": len(results),
+        "runtime_per_instance": (time.time() - start_time) / len(results),
     }
     with open(output_path / f"difuscombination_samples_{timestamp}_{config.process_idx}.json", "w") as f:
         json.dump(metadata, f)
