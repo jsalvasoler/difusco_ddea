fd5502031446906245492bdba88addbda24fc60c
Joan Salva <e12223411@conan.ac.tuwien.ac.at>
2025-03-01 15:51:14 +0100

    refactor: return results using OptimalRecombResults

--- src/problems/mis/solve_optimal_recombination.py
+++ src/problems/mis/solve_optimal_recombination.py
@@ -31,21 +31,35 @@ def get_starting_solution(instance: MISInstance, solution_1: np.array, solution_
     return starting_solution
 
 
+@dataclass
+class OptimalRecombResults:
+    children_np_labels: np.array
+    children: np.array
+    runtime: float
+    parent_1_obj: int
+    parent_2_obj: int
+    children_obj: int
+
+    def __getitem__(self, key: str) -> np.array | float | int:
+        return getattr(self, key)
+
+
 def process_mwis_result(
     mwis: MWISResult, instance: MISInstance, solution_1: np.array, solution_2: np.array, start_time: float
-) -> dict:
+) -> OptimalRecombResults:
     print(f"parent obj: {len(solution_1)}, {len(solution_2)}")
     print(f"children obj: {len(mwis.x)}")
     np_labels = np.zeros(instance.n_nodes)
     np_labels[mwis.x] = 1
 
-    return {
-        "runtime": round(time.time() - start_time, 4),
-        "parent_1_obj": len(solution_1),
-        "parent_2_obj": len(solution_2),
-        "children_obj": len(mwis.x),
-        "children_np_labels": np_labels,
-    }
+    return OptimalRecombResults(
+        children_np_labels=np_labels,
+        children=np.array(mwis.x),
+        runtime=round(time.time() - start_time, 4),
+        parent_1_obj=len(solution_1),
+        parent_2_obj=len(solution_2),
+        children_obj=len(mwis.x),
+    )
 
 
 def solve_wmis(
@@ -54,7 +68,7 @@ def solve_wmis(
     solution_2: np.array,
     time_limit: int = 60,
     **kwargs,
-) -> dict:
+) -> OptimalRecombResults:
     """
     Solve the following problem:
     max weighted MIS rewarding nodes in solution_1 and solution_2 according to lambda_penalty
@@ -96,7 +110,7 @@ def solve_constrained_mis(
     solution_2: np.array,
     time_limit: int = 60,
     **kwargs,
-) -> dict:
+) -> OptimalRecombResults:
     """
     Solve the following problem:
     max MIS
@@ -143,7 +157,7 @@ def solve_local_branching_mis(
     solution_2: np.array,
     time_limit: int = 60,
     **kwargs,
-) -> dict:
+) -> OptimalRecombResults:
     """
     Solve the following problem:
     max MIS
